90644c5d70a52a00d363c194f66cfa232e490043
KillerBLS <dm.mironov01@gmail.com>
2014-06-08 19:41:49 +0400

    Welcome.js

---
+++ Welcome.js
@@ -0,0 +1,27 @@
+var Option = {NAME: "name"};
+
+function getOptionAttr(attr){
+var sdcard = android.os.Environment.getExternalStorageDirectory();
+var mcpeDir = new java.io.File(sdcard.getAbsolutePath(), "games/com.mojang/");
+var optionsDir = new java.io.File(mcpeDir, "minecraftpe/");
+var optionsFile = new java.io.File(optionsDir, "options.txt");
+var br = new java.io.BufferedReader(new java.io.FileReader(optionsFile));
+var str, prop;
+var ln = new Array();
+
+while((str = br.readLine()) != null){
+
+ln.push(str);
+
+}
+
+i = ln.join().replace(",", ":");
+prop = i.split(":");
+
+return prop[prop.indexOf(attr) + 1];
+}
+
+function newLevel()
+{
+clientMessage("Welcome, " + ChatColor.GOLD + getOptionAttr("mp_username") + ChatColor.WHITE + "!");
+}
