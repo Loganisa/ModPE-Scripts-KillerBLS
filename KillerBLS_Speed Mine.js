e478d7da6ff9452f35edde8f4af4e4e16f19c6e0
KillerBLS <dm.mironov01@gmail.com>
2014-07-25 19:21:51 +0400

    Create KillerBLS_Speed Mine.js

---
+++ KillerBLS_Speed Mine.js
@@ -0,0 +1,6 @@
+//This is the code I did not complete, but I think you understand how to continue :)
+
+Block.setDestroyTime(1,null);
+Block.setDestroyTime(2,null);
+Block.setDestroyTime(3,null);
+//And so on to continue...
