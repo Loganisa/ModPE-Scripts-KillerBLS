9ec142d90c2958127abc0ed47b24e31fadcfa816
KillerBLS <dm.mironov01@gmail.com>
2014-08-15 14:48:48 +0400

    Delete KillerBLS_Friendly.js

--- KillerBLS_Friendly.js
+++
@@ -1,8 +0,0 @@
-function modTick() {
-             return;
-	var entitiesList = Entity.getAll();
-	for(var i = 0; i < entitiesList.length; i++) {
-		if (Player.isPlayer(entitiesList[i])) continue;
-		Entity.remove(entitiesList[i]);
-	}
-}
