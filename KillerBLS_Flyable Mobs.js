1cf832668fc03580c48ccb1c14cdf8f0411a2b28
KillerBLS <dm.mironov01@gmail.com>
2014-08-15 14:47:58 +0400

    Update KillerBLS_Flyable Mobs.js

--- KillerBLS_Flyable Mobs.js
+++ KillerBLS_Flyable Mobs.js
@@ -1,4 +1,9 @@
 var GUI;
+var GUI1;
+var GUI2;
+var GUI3;
+var GUI4;
+var GUI5;
 var t = false;
 var u = false;
 var d = false;
@@ -8,17 +13,18 @@ var l = false;
 var r = false;
 var tick = false;
 var mob;
- 
+  
 //U D F B L R
- 
+  
 function newLevel(){
- print("Flyable Mobs Mod by KillerBLS");
+ print("Flyable Mobs Remake by KillerBLS");
+ clientMessage("Keys: U - Up; D - Down;");
     var ctx = com.mojang.minecraftpe.MainActivity.currentMainActivity.get();
     ctx.runOnUiThread(new java.lang.Runnable({ run: function(){
         try{
             var layout = new android.widget.LinearLayout(ctx);
             layout.setOrientation(1);
- 
+  
             var button = new android.widget.Button(ctx);
             button.setText("U");
             button.setOnTouchListener(new android.view.View.OnTouchListener({
@@ -40,7 +46,7 @@ function newLevel(){
                 }
             }));
             layout.addView(button);
- 
+  
             GUI = new android.widget.PopupWindow(layout, android.widget.RelativeLayout.LayoutParams.WRAP_CONTENT, android.widget.RelativeLayout.LayoutParams.WRAP_CONTENT);
             GUI.setBackgroundDrawable(new android.graphics.drawable.ColorDrawable(android.graphics.Color.TRANSPARENT));
             GUI.showAtLocation(ctx.getWindow().getDecorView(), android.view.Gravity.RIGHT | android.view.Gravity.TOP, 0, 0);
@@ -48,13 +54,13 @@ function newLevel(){
             print("Error: " + err);
         }
     }}));
-     
+      
     var ctx = com.mojang.minecraftpe.MainActivity.currentMainActivity.get();
     ctx.runOnUiThread(new java.lang.Runnable({ run: function(){
         try{
             var layout = new android.widget.LinearLayout(ctx);
             layout.setOrientation(1);
- 
+  
             var button1 = new android.widget.Button(ctx);
             button1.setText("D");
             button1.setOnTouchListener(new android.view.View.OnTouchListener({
@@ -76,7 +82,7 @@ function newLevel(){
                 }
             }));
             layout.addView(button1);
- 
+  
             GUI1 = new android.widget.PopupWindow(layout, android.widget.RelativeLayout.LayoutParams.WRAP_CONTENT, android.widget.RelativeLayout.LayoutParams.WRAP_CONTENT);
             GUI1.setBackgroundDrawable(new android.graphics.drawable.ColorDrawable(android.graphics.Color.TRANSPARENT));
             GUI1.showAtLocation(ctx.getWindow().getDecorView(), android.view.Gravity.RIGHT | android.view.Gravity.TOP, 0, 80);
@@ -84,13 +90,13 @@ function newLevel(){
             print("Error: " + err);
         }
     }}));
-     
+      
     var ctx = com.mojang.minecraftpe.MainActivity.currentMainActivity.get();
     ctx.runOnUiThread(new java.lang.Runnable({ run: function(){
         try{
             var layout = new android.widget.LinearLayout(ctx);
             layout.setOrientation(1);
- 
+  
             var button2 = new android.widget.Button(ctx);
             button2.setText("F");
             button2.setOnTouchListener(new android.view.View.OnTouchListener({
@@ -112,7 +118,7 @@ function newLevel(){
                 }
             }));
             layout.addView(button2);
-            
+             
             GUI2 = new android.widget.PopupWindow(layout, android.widget.RelativeLayout.LayoutParams.WRAP_CONTENT, android.widget.RelativeLayout.LayoutParams.WRAP_CONTENT);
             GUI2.setBackgroundDrawable(new android.graphics.drawable.ColorDrawable(android.graphics.Color.TRANSPARENT));
             GUI2.showAtLocation(ctx.getWindow().getDecorView(), android.view.Gravity.RIGHT | android.view.Gravity.TOP, 0, 150);
@@ -120,13 +126,13 @@ function newLevel(){
             print("Error: " + err);
         }
     }}));
-     
+      
     var ctx = com.mojang.minecraftpe.MainActivity.currentMainActivity.get();
     ctx.runOnUiThread(new java.lang.Runnable({ run: function(){
         try{
             var layout = new android.widget.LinearLayout(ctx);
             layout.setOrientation(1);
-            
+             
             var button3 = new android.widget.Button(ctx);
             button3.setText("B");
             button3.setOnTouchListener(new android.view.View.OnTouchListener({
@@ -148,7 +154,7 @@ function newLevel(){
                 }
             }));
             layout.addView(button3);
- 
+  
             GUI3 = new android.widget.PopupWindow(layout, android.widget.RelativeLayout.LayoutParams.WRAP_CONTENT, android.widget.RelativeLayout.LayoutParams.WRAP_CONTENT);
             GUI3.setBackgroundDrawable(new android.graphics.drawable.ColorDrawable(android.graphics.Color.TRANSPARENT));
             GUI3.showAtLocation(ctx.getWindow().getDecorView(), android.view.Gravity.RIGHT | android.view.Gravity.TOP, 0, 220);
@@ -156,13 +162,13 @@ function newLevel(){
             print("Error: " + err);
         }
     }}));
-     
+      
     var ctx = com.mojang.minecraftpe.MainActivity.currentMainActivity.get();
     ctx.runOnUiThread(new java.lang.Runnable({ run: function(){
         try{
             var layout = new android.widget.LinearLayout(ctx);
             layout.setOrientation(1);
-            
+             
             var button4 = new android.widget.Button(ctx);
             button4.setText("L");
             button4.setOnTouchListener(new android.view.View.OnTouchListener({
@@ -184,7 +190,7 @@ function newLevel(){
                 }
             }));
             layout.addView(button4);
- 
+  
             GUI4 = new android.widget.PopupWindow(layout, android.widget.RelativeLayout.LayoutParams.WRAP_CONTENT, android.widget.RelativeLayout.LayoutParams.WRAP_CONTENT);
             GUI4.setBackgroundDrawable(new android.graphics.drawable.ColorDrawable(android.graphics.Color.TRANSPARENT));
             GUI4.showAtLocation(ctx.getWindow().getDecorView(), android.view.Gravity.RIGHT | android.view.Gravity.TOP, 0, 290);
@@ -192,13 +198,13 @@ function newLevel(){
             print("Error: " + err);
         }
     }}));
-     
+      
     var ctx = com.mojang.minecraftpe.MainActivity.currentMainActivity.get();
     ctx.runOnUiThread(new java.lang.Runnable({ run: function(){
         try{
             var layout = new android.widget.LinearLayout(ctx);
             layout.setOrientation(1);
-            
+             
             var button5 = new android.widget.Button(ctx);
             button5.setText("R");
             button5.setOnTouchListener(new android.view.View.OnTouchListener({
@@ -220,10 +226,10 @@ function newLevel(){
                 }
             }));
             layout.addView(button5);
- 
- 
- 
- 
+  
+  
+  
+  
             GUI5 = new android.widget.PopupWindow(layout, android.widget.RelativeLayout.LayoutParams.WRAP_CONTENT, android.widget.RelativeLayout.LayoutParams.WRAP_CONTENT);
             GUI5.setBackgroundDrawable(new android.graphics.drawable.ColorDrawable(android.graphics.Color.TRANSPARENT));
             GUI5.showAtLocation(ctx.getWindow().getDecorView(), android.view.Gravity.RIGHT | android.view.Gravity.TOP, 0, 360);
@@ -232,57 +238,47 @@ function newLevel(){
         }
     }}));
     }
- 
-function attackHook(attacker,victim)
-{
-preventDefault();
-rideAnimal(attacker,victim);
-t = true;
-tick = true;
-mob = victim;
+  
+function attackHook(attacker, victim) {
+    preventDefault();
+    rideAnimal(attacker, victim);
+    t = true;
+    tick = true;
+    mob = victim;
 }
- 
-function modTick()
-{
-if(tick==true)
-{
-if(t==true&&u==true)
-{
-setVelY(mob,0.5);
+
+function modTick() {
+    if (tick == true) {
+        if (t == true && u == true) {
+            setVelY(mob, 0.5);
+        }
+        if (t == true && d == true) {
+            setVelY(mob, -0.5);
+        }
+        if (t == true && f == true) {
+            setVelZ(mob, 0.5);
+            setVelY(mob, 0);
+        }
+        if (t == true && b == true) {
+            setVelZ(mob, -0.5);
+            setVelY(mob, 0);
+        }
+        if (t == true && l == true) {
+            setVelX(mob, 0.5);
+            setVelY(mob, 0);
+        }
+        if (t == true && r == true) {
+            setVelX(mob, -0.5);
+            setVelY(mob, 0);
+        }
+        if (r == false && f == false && l == false && b == false && d == false && u == false) {
+            setVelX(mob, 0);
+            setVelY(mob, 0);
+            setVelZ(mob, 0);
+        }
+    }
 }
-if(t==true&&d==true)
-{
-setVelY(mob,-0.5);
-}
-if(t==true&&f==true)
-{
-setVelZ(mob,0.5);
-setVelY(mob,0.01);
-}
-if(t==true&&b==true)
-{
-setVelZ(mob,-0.5);
-setVelY(mob,0.01);
-}
-if(t==true&&l==true)
-{
-setVelX(mob,0.5);
-setVelY(mob,0.01);
-}
-if(t==true&&r==true)
-{
-setVelX(mob,-0.5);
-setVelY(mob,0.01);
-}
-if(r==false&&f==false&&l==false&&b==false&&d==false&&u==false)
-{
-setVelX(mob,0);
-setVelY(mob,0);
-setVelZ(mob,0);
-}
-}
-}
- 
+  
 function leaveGame(){
     var ctx = com.mojang.minecraftpe.MainActivity.currentMainActivity.get();
     ctx.runOnUiThread(new java.lang.Runnable({ run: function(){
