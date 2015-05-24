6560f43151ea34e6f9c413668539232b0412000b
KillerBLS <dm.mironov01@gmail.com>
2015-05-24 13:08:19 +0200

    Create GUI_System.js

---
+++ GUI_System.js
@@ -0,0 +1,28 @@
+//Activity
+var this = com.mojang.minecraftpe.MainActivity.currentMainActivity.get();
+
+//Widgets
+var Button = android.widget.Button;
+var LinearLayout = android.widget.LinearLayout;
+var RelativeLayout = android.widget.RelativeLayout;
+var PopupWindow = android.widget.PopupWindow;
+var ScrollView = android.widget.ScrollView;
+var TextView = android.widget.TextView;
+var CheckBox = android.widget.CheckBox;
+var Switch = android.widget.Switch;
+var ImageView = android.widget.ImageView;
+
+//Graphics and View
+var Typeface = android.graphics.Typeface;
+var Color = android.graphics.Color;
+var View = android.view.View;
+var ColorDrawable = android.graphics.drawable.ColorDrawable;
+var Gravity = android.view.Gravity;
+var BitmapFactory = android.graphics.BitmapFactory;
+
+//Other
+var Environment = android.os.Environment;
+var File = java.io.File;
+var FileOutputStream = java.io.FileOutputStream;
+var Runnable = java.lang.Runnable;
+var Base64 = android.util.Base64;
